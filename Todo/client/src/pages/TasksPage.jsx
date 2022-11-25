7a4e77e5612a926ecab91b16b16582966e0e2f9c
Xostron <aat9044107480@gmail.com>
2022-11-25 16:32:54 +0300

    отображение списка задач (моковые данные)

--- Todo/client/src/pages/TasksPage.jsx
+++ Todo/client/src/pages/TasksPage.jsx
@@ -1,9 +1,77 @@
 import React from "react"
+import { TaskItem } from "../components/task/TaskItem"
+import { ListCol } from "../components/UI/list/list-column/ListCol"
+import iDel from '../source/icons/bx-trash-alt.svg'
+import { BtnIcon } from '../components/UI/button/btn-icon/BtnIcon'
+
+
+const tasksMock = [
+    { id: 1, title: 'task1', date_create: '25.11.22', date_finish: '29.11.22' },
+    { id: 2, title: 'task2', date_create: '25.11.22', date_finish: '29.11.22' },
+    { id: 3, title: 'task3', date_create: '25.11.22', date_finish: '29.11.22' },
+    { id: 4, title: 'task4', date_create: '25.11.22', date_finish: '29.11.22' },
+    { id: 5, title: 'task5', date_create: '25.11.22', date_finish: '29.11.22' },
+    { id: 6, title: 'task6', date_create: '25.11.22', date_finish: '29.11.22' },
+    { id: 7, title: 'task7', date_create: '25.11.22', date_finish: '29.11.22' },
+    { id: 8, title: 'task8', date_create: '25.11.22', date_finish: '29.11.22' },
+    { id: 9, title: 'task9', date_create: '25.11.22', date_finish: '29.11.22' },
+    { id: 10, title: 'task10', date_create: '25.11.22', date_finish: '29.11.22' },
+    { id: 11, title: 'task11', date_create: '25.11.22', date_finish: '29.11.22' },
+    { id: 12, title: 'task12', date_create: '25.11.22', date_finish: '29.11.22' },
+    { id: 13, title: 'task13', date_create: '25.11.22', date_finish: '29.11.22' },
+    { id: 14, title: 'task14', date_create: '25.11.22', date_finish: '29.11.22' },
+    { id: 15, title: 'task15', date_create: '25.11.22', date_finish: '29.11.22' },
+]
 
 export const TasksPage = () => {
+
+    // ********************************PROPS********************************
+    // функция, которая возвращает набор кнопок для одной задачи objNameTask
+    const setBtnsTask = (nameTask) => {
+        return ([
+            {
+                icon: iDel,
+                handler: (e) => {
+                    console.log('del ', nameTask)
+                    e.stopPropagation()
+                }
+            },])
+    }
+
+    // callback для map: формирует props 
+    const itemsFunc = (item, idx) => {
+        return ({
+            id: item.id || idx,
+            title: item.title,
+            date_create: item.date_create,
+            date_finish: item.date_finish,
+            handlerOpen: () => { console.log('open ', item.title) },
+            tools: setBtnsTask(item.title),
+        })
+    }
+
+    // callback map для компонента ListCol
+    const renderItem = (task, idx) => {
+        return (
+            <TaskItem key={idx} item={task}>
+                {task.tools.map((btn, idx) => {
+                    return (
+                        <BtnIcon key={idx} icon={btn.icon} handler={btn.handler} />
+                    )
+                })}
+            </TaskItem>)
+    }
+
+    // получаем массив задач: данные с сервера + handler
+    const tasks = tasksMock.map(itemsFunc)
+
+
     return (
         <div>
-            Tasks
+            <ListCol
+                item={tasks}
+                renderItem={renderItem}
+            />
         </div>
     )
 }
