2fb49a8428ca792988494cbc61580ed9a56e9b2a
Xostron <aat9044107480@gmail.com>
2022-11-27 18:10:23 +0300

    кнопка иконка

--- Todo/client/src/components/UI/button/btn-icon/BtnIcon.jsx
+++ Todo/client/src/components/UI/button/btn-icon/BtnIcon.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import style from './BtnIcon.module.less'
 import { HandySvg } from 'handy-svg'
 
-export const BtnIcon = ({ icon, handler }) => {
+export const BtnIcon = ({ icon, handler, width = 24, height = 24, children }) => {
     return (
         <div className={style.container} onClick={handler}>
-            <HandySvg className={style.icon} src={icon} />
+            {children}
+            <HandySvg className={style.icon} src={icon} width={width} height={height} />
         </div>
     )
 }
