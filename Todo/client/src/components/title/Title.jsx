b0c90e5fe54cf1588f1b25f5fc98084cdd3d279d
Xostron <aat9044107480@gmail.com>
2022-11-27 10:15:39 +0300

    init component Title

---
+++ Todo/client/src/components/title/Title.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { HandySvg } from 'handy-svg'
+import style from './Title.module.less'
+
+
+export const Title = ({ children, props }) => {
+    const {
+        icon1,
+        handler1,
+        title,
+        text
+    } = props
+    return (
+        <div className={style.title}>
+            <div className={style.left}>
+                {
+                    icon1 ? <div className={style.btn} onClick={handler1}>
+                        <HandySvg
+                            src={icon1}
+                            className={style.icon}
+                        />
+                    </div> :
+                        null
+                }
+
+
+                <strong>
+                    {title}
+                    <span className={style.text_span}>{text}</span>
+                </strong>
+
+            </div>
+
+
+
+            <div className={style.right}>
+                {children}
+            </div>
+        </div>
+    )
+}
