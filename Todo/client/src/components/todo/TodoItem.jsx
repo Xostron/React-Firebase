07f3e74079db6546d60fd1d9ef4d1ca65008dc54
Xostron <aat9044107480@gmail.com>
2022-11-28 13:11:22 +0300

    style

--- Todo/client/src/components/todo/TodoItem.jsx
+++ Todo/client/src/components/todo/TodoItem.jsx
@@ -3,15 +3,18 @@ import { InputTodo } from "../UI/input/input-text-todo/InputTodo";
 import { MyCheckbox } from '../UI/check/MyCheckbox'
 import style from './TodoItem.module.less'
 import iHandler from '../../source/icons/bx-trash-alt.svg'
+import { MyTextarea } from "../UI/input/areatext/MyTextarea";
 export const TodoItem = ({ props }) => {
     const {
         propsInput,
-        propsCheck
+        propsCheck,
+        propsTextarea
     } = props
     return (
         <div className={style.container}>
             <MyCheckbox props={propsCheck} />
-            <InputTodo props={propsInput} />
+            {/* <InputTodo props={propsInput} /> */}
+            <MyTextarea props={propsTextarea} />
         </div>
     )
 }
