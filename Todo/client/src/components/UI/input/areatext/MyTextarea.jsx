ce22b5ec00cca1549ffc9fa82e1c8dd1864f3615
Xostron <aat9044107480@gmail.com>
2022-11-28 13:09:47 +0300

    add autosize

--- Todo/client/src/components/UI/input/areatext/MyTextarea.jsx
+++ Todo/client/src/components/UI/input/areatext/MyTextarea.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import style from './MyTextarea.module.less'
 import { HandySvg } from 'handy-svg'
+import { useRef } from "react";
+import { useEffect } from "react";
+import { useLayoutEffect } from "react";
+import { useState } from "react";
 
 export const MyTextarea = ({ props }) => {
     const {
@@ -10,18 +14,51 @@ export const MyTextarea = ({ props }) => {
         value,
         autofocus,
     } = props
+
+    const ref = useRef()
+    const [rows, setRows] = useState(0)
+
+
+    const handler = (e) => {
+        changeHandler()
+        setRows(ref.current.scrollHeight)
+    }
+
+    useEffect(() => {
+        let str = ref.current.style.height.match(/[0-9]/g) || []
+        let val = Number(str.join(''))
+        // console.log('test = ', val, ref.current.scrollHeight)
+        if (ref.current.scrollHeight > val) {
+            ref.current.style.height = `${ref.current.scrollHeight}px`;
+            console.log('if = ', val, rows)
+        }
+        else if (val === (ref.current.scrollHeight + 1)) {
+            if (ref.current.value === '') {
+                ref.current.style.height = '39px'
+            }
+            else {
+                ref.current.style.height = `${ref.current.scrollHeight - 20}px`;
+                console.log('else = ', ref.current.scrollHeight, rows)
+            }
+        }
+    }, [rows])
+
+    useEffect(() => {
+        ref.current.style.height = '39px'
+    }, [])
+
     return (
         <div className={style.container}>
             <textarea
                 className={style.myTextarea}
-                // ref={areaRef}
                 type="text"
                 autoComplete='off'
                 autoFocus={autofocus || false}
                 placeholder={placeholder}
                 name={name}
-                onChange={changeHandler}
+                onChange={handler}
                 value={value}
+                ref={ref}
             />
         </div>
     )
